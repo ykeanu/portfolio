2083c18bb0da028a9c3156e033f0cf8db4caca6a
ykeanu <ykhernan@ucla.edu>
2018-01-02 18:36:59 -0800

    Refactor components + Created Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import smoothScroll from 'smoothscroll-polyfill';
+import 'css/index.scss';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const self = this;
+    smoothScroll.polyfill();
+
+    document.addEventListener('DOMContentLoaded', function(event) {
+      self.refs.navbar.addEventListener('click', function(e) {
+        if (e.target.innerText === 'Projects') {
+          document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+        } else if (e.target.innerText === 'Contact') {
+          document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
+  }
+
+  render() {
+    return (
+      <nav className="navbar" ref="navbar">
+        <a href="https://justinchi.me">Home</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import styled from 'styled-components';
 
 export default class Home extends Component {
   static propTypes = {};
@@ -7,7 +8,15 @@ export default class Home extends Component {
     return (
       <div>
         <h1>Izzy Keanu's Home!</h1>
+        <Image src={require('../assets/images/headshot.jpg')} alt='Izzy Keanus headshot' />
+        <div className="hero-description">
+        Recent UCLA graduate with industry experience in big data for healthcare and full-stack software development. Strong experience working with Javascript, Node.js, React, React Native, C#, MySQL, MongoDB and web/iOS/Android development.
+        </div>
       </div>
     );
   }
 }
+
+const Image = styled.img`
+  width: 100%;
+`;
